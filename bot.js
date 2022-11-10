cd8c4e4aa7f0c557c51fb7250b0a87e8864cc0bc
Nikita Berdyev <naberdyev@gmail.com>
2022-11-10 19:14:47 +0400

    feat: new api for covid stats

--- bot.js
+++ bot.js
@@ -1,42 +1,41 @@
-require('dotenv').config()
-const { Telegraf } = require('telegraf')
-const api = require('corona-info')
-const Markup = require('telegraf').Markup
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+const api = require('novelcovid');
+const Markup = require('telegraf').Markup;
 const COUNTRIES_LIST = require('./constants');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name}!
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) =>
+  ctx.reply(
+    `Привет ${ctx.message.from.first_name}!
 Узнай статистику по коронавирусу.
 Введи на английском название страны и получи статистику.
 Посмотреть весь список стран можно командой /help
-`, Markup.keyboard([
-    ['US', 'Russia'],
-    ['Ukraine', 'Kazakhstan']
+`,
+    Markup.keyboard([
+      ['US', 'Russia'],
+      ['Ukraine', 'Kazakhstan'],
     ]).resize()
-    )
+  )
 );
 
 bot.help((ctx) => ctx.reply(COUNTRIES_LIST));
 
 bot.on('text', async (ctx) => {
-    let data = {};
-    
-    
+  let data = {};
 
-    try {
-
+  try {
     ///api///
     const covidData = async () => {
-        let data = await api.findData({ country: ctx.message.text });
-        return data;
+      let data = await api.countries({ country });
+      return data;
     };
     ////api////
-    
-  
+
     async function go() {
-        let data = await covidData();
-        const formatData = `
-Данные на ${data.updatedDate} в ${data.countryName}:
+      let data = await covidData();
+      const formatData = `
+Данные на сегодня в ${data.country}:
 Всего случаев: ${data.cases}
 Всего выздоровело: ${data.recovered}
 Всего смертей: ${data.deaths}
@@ -44,14 +43,14 @@ bot.on('text', async (ctx) => {
 Выздоровело сегодня: ${data.todayRecovered}
 Умерло сегодня: ${data.todayDeaths}
 `;
-ctx.reply(formatData);
-ctx.replyWithPhoto({ url: data.countryFlag });
+      ctx.reply(formatData);
+      ctx.replyWithPhoto({ url: data.countryFlag });
     }
-    go()
-        } catch {
-        console.log('Ошибка');
-        ctx.reply('Ошибка. Такой страны не существует! Посмотрите /help');
-        };
+    go();
+  } catch {
+    console.log('Ошибка');
+    ctx.reply('Ошибка. Такой страны не существует! Посмотрите /help');
+  }
 });
-bot.launch()
+bot.launch();
 console.log('Бот запущен');
